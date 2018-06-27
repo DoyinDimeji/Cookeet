b35956e605d5cd161baebda279c9a51f6dac5006
DoyinDimeji <Oladimeji.Oladoyin@gmail.com>
2018-06-28 00:42:48 +0100

    Android splashscreen

--- Cookeet/Cookeet.Android/MainActivity.cs
+++ Cookeet/Cookeet.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Cookeet.Droid
 {
-    [Activity(Label = "Cookeet", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Cookeet", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
--- Cookeet/Cookeet/MainPage.xaml.cs
+++ Cookeet/Cookeet/MainPage.xaml.cs
@@ -7,7 +7,7 @@ using Xamarin.Forms;
 
 namespace Cookeet
 {
-	public partial class MainPage : ContentPage
+	public partial class MainPage : MasterDetailPage
 	{
 		public MainPage()
 		{
